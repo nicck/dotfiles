09dcde03423d73b9720e85b44ee4749c921f7eb2
Nickolay Abdrafikov <nicck.olay@gmail.com>
2014-02-22 12:19:53 +0400

    responsive bash prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,6 +1,40 @@
-# /usr/share/big_dir_name -> ../share/big_dir_name if pwdmaxlen=20
-set_bash_prompt_command () {
-  local pwdmaxlen=25
+git_prompt=/usr/local/etc/bash_completion.d/git-prompt.sh
+[ -f $git_prompt ] && source $git_prompt
+
+# regular colors
+BLACK='\[\033[0;30m\]'
+RED='\[\033[0;31m\]'
+GREEN='\[\033[0;32m\]'
+YELLOW='\[\033[0;33m\]'
+BLUE='\[\033[0;34m\]'
+MAGENTA='\[\033[0;35m\]'
+CYAN='\[\033[0;36m\]'
+WHITE='\[\033[0;37m\]'
+
+# empahsized (bolded) colors
+EMBLACK='\[\033[1;30m\]'
+EMRED='\[\033[1;31m\]'
+EMGREEN='\[\033[1;32m\]'
+EMYELLOW='\[\033[1;33m\]'
+EMBLUE='\[\033[1;34m\]'
+EMMAGENTA='\[\033[1;35m\]'
+EMCYAN='\[\033[1;36m\]'
+EMWHITE='\[\033[1;37m\]'
+
+# background colors
+BGBLACK='\[\033[40m\]'
+BGRED='\[\033[41m\]'
+BGGREEN='\[\033[42m\]'
+BGYELLOW='\[\033[43m\]'
+BGBLUE='\[\033[44m\]'
+BGMAGENTA='\[\033[45m\]'
+BGCYAN='\[\033[46m\]'
+BGWHITE='\[\033[47m\]'
+
+COLOREND='\[\033[0m\]'
+
+working_directory() {
+  local pwdmaxlen=$(tput cols)/5
   local trunc_symbol=".."
   local dir=${PWD##*/}
 
@@ -15,44 +49,12 @@ set_bash_prompt_command () {
     NEW_PWD=${NEW_PWD:$pwdoffset:$pwdmaxlen}
     NEW_PWD=${trunc_symbol}/${NEW_PWD#*/}
   fi
+
+  echo -e "${EMBLUE}${NEW_PWD}${COLOREND}"
 }
 
-set_bash_prompt() {
-  local NONE='\[\033[0m\]'    # unsets color to term's fg color
-
-  # regular colors
-  local K='\[\033[0;30m\]'    # black
-  local R='\[\033[0;31m\]'    # red
-  local G='\[\033[0;32m\]'    # green
-  local Y='\[\033[0;33m\]'    # yellow
-  local B='\[\033[0;34m\]'    # blue
-  local M='\[\033[0;35m\]'    # magenta
-  local C='\[\033[0;36m\]'    # cyan
-  local W='\[\033[0;37m\]'    # white
-
-  # empahsized (bolded) colors
-  local EMK='\[\033[1;30m\]'
-  local EMR='\[\033[1;31m\]'
-  local EMG='\[\033[1;32m\]'
-  local EMY='\[\033[1;33m\]'
-  local EMB='\[\033[1;34m\]'
-  local EMM='\[\033[1;35m\]'
-  local EMC='\[\033[1;36m\]'
-  local EMW='\[\033[1;37m\]'
-
-  # background colors
-  local BGK='\[\033[40m\]'
-  local BGR='\[\033[41m\]'
-  local BGG='\[\033[42m\]'
-  local BGY='\[\033[43m\]'
-  local BGB='\[\033[44m\]'
-  local BGM='\[\033[45m\]'
-  local BGC='\[\033[46m\]'
-  local BGW='\[\033[47m\]'
-
-  local UC=$EMK                 # user's color
-  [ $UID -eq "0" ] && UC=$EMR   # root's color
-
+git_state() {
+  GIT_PS1_SHOWCOLORHINTS=1
   GIT_PS1_SHOWDIRTYSTATE=1
   GIT_PS1_SHOWSTASHSTATE=1
   GIT_PS1_SHOWUNTRACKEDFILES=1
@@ -61,14 +63,22 @@ set_bash_prompt() {
   command -v __git_ps1 > /dev/null
   if [ $? -eq 0 ]
   then
-    PS1="${BGW}${UC}\t${NONE} ${EMB}\${NEW_PWD}${EMM}\$(__git_ps1 ' (%s)') ${NONE}\\$ ${NONE}"
-  else
-    PS1="${BGW}${UC}\t${NONE} ${EMB}\${NEW_PWD} ${NONE}\\$ ${NONE}"
+    echo "${EMMAGENTA}$(__git_ps1 ' (%s)')${COLOREND}"
   fi
 }
 
-git_prompt=/usr/local/etc/bash_completion.d/git-prompt.sh
-[ -f $git_prompt ] && source $git_prompt
+prompt_time() {
+  echo "${BGWHITE}${EMBLACK}\t${COLOREND}"
+}
 
-PROMPT_COMMAND=set_bash_prompt_command
-set_bash_prompt; unset set_bash_prompt
+prompt() {
+  if [[ $? -eq 0 ]]; then
+    exit_status="${GREEN} › ${COLOREND}"
+  else
+    exit_status="${RED} › ${COLOREND}"
+  fi
+
+  PS1="$(prompt_time) $(working_directory)$(git_state)${exit_status}"
+}
+
+PROMPT_COMMAND=prompt
