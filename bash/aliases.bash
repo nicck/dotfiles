1746b1f72e54f1edba6979c71af89c7ddc1acb9a
Nickolay Abdrafikov <nicck.olay@gmail.com>
2014-10-15 15:24:04 +0400

    more varians of gif-optimize alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,5 +1,6 @@
 alias ll='ls -hla'
 alias reload!='. ~/.profile'
 alias binstubs='export PATH=./bin:${PATH%:./bin}'
-alias gif-optimize='gifsicle --colors=128 --optimize=3'
+alias gif-optimize-64='gifsicle --colors=64 --optimize=3 '
+alias gif-optimize-32='gifsicle --colors=32 --optimize=3'
 alias cask='brew cask'
