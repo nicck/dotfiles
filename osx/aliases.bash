d3a68525a813c54ce6a8ac6e4b745be126e1d105
Nickolay Abdrafikov <nicck.olay@gmail.com>
2015-03-19 19:43:36 +0300

    add aliases for memcache stats, wifi restart, and dns flush

--- osx/aliases.bash
+++ osx/aliases.bash
@@ -1,3 +1,5 @@
 # brew install afsctool
 alias packfs='afsctool -c -7 -m 10485760 -s 10 -v'
 alias packfs!='sudo afsctool -c -7 -m 10485760 -s 10 -v'
+alias wifi='networksetup -setairportpower airport off && networksetup -setairportpower airport on'
+alias dns-flush='sudo killall -HUP mDNSResponder'
