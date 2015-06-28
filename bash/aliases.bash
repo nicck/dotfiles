8dd2531ed4586d4d260eb52f39006317d275d321
Nickolay Abdrafikov <nicck.olay@gmail.com>
2015-06-28 13:48:05 +0300

    add fix-upwork-app alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -6,3 +6,4 @@ alias gif-optimize-32='gifsicle --colors=32 --optimize=3'
 alias cask='brew cask'
 alias memcache-stats='echo stats | nc 127.0.0.1 11211'
 alias memcache-stats-watch='watch -d "echo stats | nc 127.0.0.1 11211 | grep -E \"get|set|miss|hit\""'
+alias fix-upwork-app="/usr/libexec/PlistBuddy -c 'Add :NSSupportsAutomaticGraphicsSwitching bool YES' /Applications/Upwork.app/Contents/Info.plist"
