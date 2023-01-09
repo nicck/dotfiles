07d813054abcb6496ddd3a187daa29cb3785fb39
Nikolai Abdrafikov <nicck.olay@gmail.com>
2023-01-09 14:10:04 +0000

    use git-delta-musl (statically-linked, better compatibility)

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ cp .zshrc $HOME/.zshrc
 cp .gitignore $HOME/.gitignore
 sed "s:HOME_DIR:$HOME:g" .gitconfig > $HOME/.gitconfig
 
-curl -L https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb -o git-delta_0.15.1_amd64.deb
-sudo dpkg -i git-delta_0.15.1_amd64.deb
-rm git-delta_0.15.1_amd64.deb
+curl -L -o file.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb \
+    && sudo dpkg -i file.deb && rm file.deb
