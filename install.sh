7da5c0d3cb5c82364ba9fc24a9d856d1f61b4807
Nikolai Abdrafikov <nicck.olay@gmail.com>
2023-01-09 14:22:48 +0000

    override existing .zshrc and .gitignore

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-cp .zshrc $HOME/.zshrc
-cp .gitignore $HOME/.gitignore
+cp -f .zshrc $HOME/.zshrc
+cp -f .gitignore $HOME/.gitignore
 sed "s:HOME_DIR:$HOME:g" .gitconfig > $HOME/.gitconfig
 
 curl -L -o file.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb \
