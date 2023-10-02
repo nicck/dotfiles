e02b6e5bbd9482cee066ac6263fa5856cd09cc52
Nikolai Abdrafikov <nicck.olay@gmail.com>
2023-10-02 10:45:29 +0000

    add nushell installation

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sed "s:HOME_DIR:$HOME:g" .gitconfig > $HOME/.gitconfig
 
 curl -L -o file.deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb \
     && sudo dpkg -i file.deb && rm file.deb
+
+curl -SL -o nu-shell.tar.gz https://github.com/nushell/nushell/releases/download/0.85.0/nu-0.85.0-x86_64-unknown-linux-gnu.tar.gz \
+    && tar -xf nu-shell.tar.gz && rm nu-shell.tar.gz && sudo cp nu-shell/nu /usr/local/bin/
